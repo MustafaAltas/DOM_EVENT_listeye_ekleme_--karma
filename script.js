6d4ee68131d5318ea8ffc2e4b3315966aa0a1f9e
D1183-Mustafa <93746275+D1183-Mustafa@users.noreply.github.com>
2022-02-08 04:31:16 +0300

    js dosyasında listeye eleman ekleme işlemleri için kod yazıldı

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const formVeri = document.querySelector(".form");
 const inputText = document.querySelector(".isim");
 const inputYas = document.querySelector(".yaş");
+const liste = document.querySelector(".sağ_div2_ul");
 
 kaydet();
 
@@ -15,6 +16,7 @@ function veriAlma(event) {
     console.log(inputYas.value);
     event.preventDefault();
 
+    //***Span Tag */
     const span1 = document.createElement("span");
     const span2 = document.createElement("span");
 
@@ -24,7 +26,37 @@ function veriAlma(event) {
     span1.appendChild(document.createTextNode(inputText.value));
     span2.appendChild(document.createTextNode(inputYas.value));
 
-    console.log(span1);
+    ///*** a tag */
+
+    const a = document.createElement("a");
+    a.setAttribute("href","#");
+    a.setAttribute("class","sağ_div2_ul_li_a");
+
+    //***img oluşturma */
+
+    const img = document.createElement("img");
+    img.setAttribute("class","sağ_div2_ul_li_img");
+    img.setAttribute("src","https://img.icons8.com/wired/30/000000/cancel--v1.png");
+
+    //******** */
+    a.appendChild(img);
+
+    console.log(a);
+
+    //**li tag */
+
+    const li = document.createElement("li");
+    li.setAttribute("class","sağ_div2_ul_li");
+
+    //****ul tag */
+
+    li.appendChild(span1);
+    li.appendChild(span2);
+    li.appendChild(a);
+    console.log(li);
+
+    liste.appendChild(li);
+
 }
 
 
